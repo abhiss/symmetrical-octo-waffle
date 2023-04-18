1f3441f603120cc3608fbfc98599351294157d90
Dominic Quintero <dominicaq09@gmail.com>
2023-04-18 13:02:22 -0700

    Player movement animations

--- Assets/Scripts/Character/TopDownCharacterMotor.cs
+++ Assets/Scripts/Character/TopDownCharacterMotor.cs
@@ -6,21 +6,55 @@ public class TopDownCharacterMotor : MonoBehaviour
 {
     // TODO: Get instance of input manager
     public GameObject cameraObject;
+    public GameObject playerModel;
     private TemporaryCameraScript cam;
     private TopDownCharacterLogic controller;
 
+    [Header("Animatons")]
+    public float dampTime = 0.1f;
+    private Animator animator;
+    private int horizontalHash;
+    private int verticalHash;
+    private Vector3 animationDir;
+
     void Start()
     {
+        if (playerModel == null || cameraObject == null)
+        {
+            Debug.LogError(
+                "Empty fields in TopDownCharaterMotor script. " +
+                "aborted Start() on "
+                + transform.name + " gameobject");
+            return;
+        }
+
+        // Movement + Camera
         cam = cameraObject.GetComponent<TemporaryCameraScript>();
         controller = GetComponent<TopDownCharacterLogic>();
         controller.Init();
+
+        // Animations
+        animator = playerModel.GetComponent<Animator>();
+        horizontalHash = Animator.StringToHash("Horizontal");
+        verticalHash = Animator.StringToHash("Vertical");
     }
 
     void Update()
     {
-        controller.inputVelocity = controller.ProcessRawInput(GetInput());
+        Vector3 input = GetInput();
+        controller.inputVelocity = controller.ProcessRawInput(input);
         controller.RotateCharacter(cam.CursorWorldSpacePosition);
         controller.Tick();
+
+        // Animatons
+        Vector3 forward = transform.forward.normalized;
+        Vector3 right = transform.right.normalized;
+        forward *= input.z;
+        right *= input.x;
+
+        animationDir = forward + right;
+        animator.SetFloat(horizontalHash, animationDir.x, dampTime, Time.deltaTime);
+        animator.SetFloat(verticalHash, animationDir.z, dampTime, Time.deltaTime);
     }
 
     private Vector3 GetInput()
@@ -31,5 +65,12 @@ public class TopDownCharacterMotor : MonoBehaviour
             0,
             -Input.GetAxis("Vertical")
         );
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draws a blue line from this transform to the target
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + animationDir, 0.1f);
     }
 }

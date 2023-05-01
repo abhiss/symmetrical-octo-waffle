98b3a5171670fbf7854b316ba5471301e0030917
TSYW01 <129975299+TSYW01@users.noreply.github.com>
2023-04-30 20:19:08 -0700

    host/connecting

---
+++ Assets/NetworkManager/ConnectionNotificationManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Unity.Netcode;
+
+/// <summary>
+/// Only attach this example component to the NetworkManager GameObject.
+/// This will provide you with a single location to register for client 
+/// connect and disconnect events.  
+/// </summary>
+public class ConnectionNotificationManager : MonoBehaviour
+{
+    public static ConnectionNotificationManager Singleton { get; internal set; }
+
+    public enum ConnectionStatus
+    {
+        Connected,
+        Disconnected
+    }
+
+    /// <summary>
+    /// This action is invoked whenever a client connects or disconnects from the game.
+    ///   The first parameter is the ID of the client (ulong).
+    ///   The second parameter is whether that client is connecting or disconnecting.
+    /// </summary>
+    public event Action<ulong, ConnectionStatus> OnClientConnectionNotification;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            // As long as you aren't creating multiple NetworkManager instances, throw an exception.
+            // (***the current position of the callstack will stop here***)
+            throw new Exception($"Detected more than one instance of {nameof(ConnectionNotificationManager)}! " +
+                $"Do you have more than one component attached to a {nameof(GameObject)}");
+        }
+        Singleton = this;
+    }
+
+    private void Start()
+    {
+        if (Singleton != this)
+        {
+            return; // so things don't get even more broken if this is a duplicate >:(
+        }
+
+        if (NetworkManager.Singleton == null)
+        {
+            // Can't listen to something that doesn't exist >:(
+            throw new Exception($"There is no {nameof(NetworkManager)} for the {nameof(ConnectionNotificationManager)} to do stuff with! " +
+                $"Please add a {nameof(NetworkManager)} to the scene.");
+        }
+
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
+    }
+
+    private void OnDestroy()
+    {
+        // Since the NetworkManager can potentially be destroyed before this component, only 
+        // remove the subscriptions if that singleton still exists.
+        if (NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnectCallback;
+        }
+    }
+
+    private void OnClientConnectedCallback(ulong clientId)
+    {
+        OnClientConnectionNotification?.Invoke(clientId, ConnectionStatus.Connected);
+    }
+
+    private void OnClientDisconnectCallback(ulong clientId)
+    {
+        OnClientConnectionNotification?.Invoke(clientId, ConnectionStatus.Disconnected);
+    }
+}
---
+++ Assets/NetworkManager/NetworkManagerUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button server_button;
+    [SerializeField] private Button host_button;
+    [SerializeField] private Button client_button;
+
+
+    private void Start()
+    {
+        NetworkManager.Singleton.StartHost();        // Starts the NetworkManager as both a server and a client (that is, has local client)
+
+    }
+
+
+    private void Awake()
+    {
+        server_button.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+
+        host_button.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        client_button.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+
+    }
+
+}
+

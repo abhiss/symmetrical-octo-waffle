9aa7bf92c70bde0e01c93c7345868694c0eab771
Dominic Quintero <dominicaq09@gmail.com>
2023-05-02 16:16:51 -0700

    Project cleanup

--- Assets/Scripts/User Interface/CinematicPan.cs
+++ Assets/Scripts/User Interface/CinematicPan.cs
@@ -6,12 +6,9 @@ public class CinematicPan : MonoBehaviour
 {
     public GameObject lookAtTarget;
     public float panSpeed = 1.0f;
-    private float bounce = 0.0f;
 
     void Update()
     {
-        //bounce = Mathf.MoveTowards(bounce, Mathf.Sin(Time.time), Time.deltaTime * panSpeed);
         transform.LookAt(lookAtTarget.transform.position);
-        //transform.RotateAround(lookAtTarget.transform.position, new Vector3(bounce, 0, 0), Time.deltaTime * panSpeed);
     }
 }

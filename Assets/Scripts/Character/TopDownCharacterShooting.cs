008ae59bd7400f5b8b53c7d97696bd0a255540ff
Dominic Quintero <dominicaq09@gmail.com>
2023-05-16 15:36:55 -0700

    Updated Ammo Handling

--- Assets/Scripts/Character/TopDownCharacterShooting.cs
+++ Assets/Scripts/Character/TopDownCharacterShooting.cs
@@ -12,7 +12,7 @@ public class TopDownCharacterShooting : MonoBehaviour
     public LayerMask EnemyMask;
     private Vector3 _aimDirection;
     private Ray _cameraRay;
-    private bool _canShoot = true;
+    private bool _shootEnabled = true;
 
     [Header("Aim Assist")]
     public bool EnableAimAssist = true;
@@ -30,6 +30,10 @@ public class TopDownCharacterShooting : MonoBehaviour
 
     private void Start()
     {
+        // Weapon Init
+        CurrentWeapon.CurrentClipSize = CurrentWeapon.MaxClipSize;
+        CurrentWeapon.CurrentAmmo = CurrentWeapon.MaxAmmo - CurrentWeapon.MaxClipSize;
+
         // Laser
         _laserLine = gameObject.AddComponent<LineRenderer>();
         _laserLine.material = LaserMaterial;
@@ -50,18 +54,31 @@ public class TopDownCharacterShooting : MonoBehaviour
         _cameraRay = Camera.main.ScreenPointToRay(mousePosition);
         _aimDirection = CalculateDirection();
 
+        if (_shootEnabled) {
+            Debug.Log(CurrentWeapon.CurrentClipSize + " / " + CurrentWeapon.CurrentAmmo);
+        }
+
+        // Reloading
+        bool reloadConditions = _shootEnabled && CurrentWeapon.CurrentClipSize != CurrentWeapon.MaxClipSize && CurrentWeapon.CurrentAmmo > 0;
+        if (Input.GetKeyDown(KeyCode.R) && reloadConditions)
+        {
+            StartCoroutine(ReloadDelay());
+        }
+
         // Shooting
         IsAiming = false;
 
         // Automatic or single fire
         bool inputFire = Input.GetKeyDown(KeyCode.Mouse0);
-        if (CurrentWeapon.Interval > 0.0f) {
+        if (CurrentWeapon.FireRate > 0.0f)
+        {
             inputFire = Input.GetKey(KeyCode.Mouse0);
         }
 
-        if (inputFire && _canShoot)
+        bool shootingConditions =  _shootEnabled && CurrentWeapon.CurrentClipSize > 0;
+        if (inputFire && shootingConditions)
         {
-            StartCoroutine(AutomaticFire());
+            StartCoroutine(FireRate());
             IsAiming = true;
             Shoot();
         }
@@ -80,6 +97,7 @@ public class TopDownCharacterShooting : MonoBehaviour
 
     private void Shoot()
     {
+        // Shoot one bullet
         StartCoroutine(GunVFX());
         if (Physics.Raycast(transform.position, _aimDirection, out RaycastHit hit, CurrentWeapon.MaxDistance))
         {
@@ -95,6 +113,8 @@ public class TopDownCharacterShooting : MonoBehaviour
                 healthSystem.TakeDamage(gameObject, CurrentWeapon.Damage);
             }
         }
+
+        --CurrentWeapon.CurrentClipSize;
     }
 
     private Vector3 AdjustCursorPostion()
@@ -169,10 +189,35 @@ public class TopDownCharacterShooting : MonoBehaviour
         _gunLight.enabled = false;
     }
 
-    private IEnumerator AutomaticFire()
+    private IEnumerator FireRate()
     {
-        _canShoot = false;
-        yield return new WaitForSeconds(CurrentWeapon.Interval);
-        _canShoot = true;
+        _shootEnabled = false;
+        yield return new WaitForSeconds(CurrentWeapon.FireRate);
+        _shootEnabled = true;
+    }
+
+    private IEnumerator ReloadDelay()
+    {
+        Debug.Log("Reloading...");
+        _shootEnabled = false;
+
+        // Update ammo pool
+        int currentBulletAmount = CurrentWeapon.CurrentAmmo + CurrentWeapon.CurrentClipSize;
+        int newBulletAmount = currentBulletAmount - CurrentWeapon.MaxClipSize;
+        if (newBulletAmount < 0) {
+            newBulletAmount = 0;
+        }
+
+        // Update clip
+        int newClip = Mathf.Min(CurrentWeapon.MaxClipSize, currentBulletAmount);
+        CurrentWeapon.CurrentClipSize = 0;
+
+        yield return new WaitForSeconds(CurrentWeapon.ReloadTime);
+
+        CurrentWeapon.CurrentAmmo = newBulletAmount;
+        CurrentWeapon.CurrentClipSize = newClip;
+
+        _shootEnabled = true;
+        Debug.Log("Reloading... Done");
     }
 }
--- Assets/Scripts/Weapons/WeaponManager.cs
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 public class WeaponManager : ScriptableObject
 {
     public float Damage = 1.0f;
-    public float Interval = 0.0f;
+    public float FireRate = 0.0f;
+    public int CurrentClipSize = 0;
+    public int MaxClipSize = 30;
+    public int CurrentAmmo = 0;
+    public int MaxAmmo = 300;
+    public float ReloadTime = 1.0f;
     public float MaxDistance = 100.0f;
 }

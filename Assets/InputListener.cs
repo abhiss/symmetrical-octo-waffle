0c3b4924967671614dfd78fe0a0c369d6d538c49
Dominic Quintero <dominicaq09@gmail.com>
2023-05-19 09:42:20 -0700

    Slightly more refined interactables

---
+++ Assets/InputListener.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class InputListener : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool DisableInput = false;
+
+    [Header("Live Variables")]
+    public bool UseKey = false;
+    public bool ReloadKey = false;
+    public bool FireKeyDown = false;
+    public bool FireKey = false;
+    public bool AltFire = false;
+    public bool AltFireDown = false;
+    public Vector3 MousePosition;
+
+    public Vector3 GetAxisInput()
+    {
+        if (DisableInput)
+        {
+            return Vector3.zero;
+        }
+
+        Vector3 rawInput = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+        return rawInput;
+    }
+
+    private void Update()
+    {
+        // TODO: This should really be a singleton. But I dont think we have the time.
+        if (DisableInput)
+        {
+            UseKey = false;
+            ReloadKey = false;
+            FireKey = false;
+            FireKeyDown = false;
+            AltFire = false;
+            AltFireDown = false;
+            return;
+        }
+
+        MousePosition = Input.mousePosition;
+
+        UseKey = Input.GetKeyDown(KeyCode.E);
+        ReloadKey = Input.GetKeyDown(KeyCode.R);
+
+        FireKeyDown = Input.GetKeyDown(KeyCode.Mouse0);
+        FireKey = Input.GetKey(KeyCode.Mouse0);
+
+        AltFire = Input.GetKey(KeyCode.Mouse1);
+        AltFireDown = Input.GetKeyDown(KeyCode.Mouse1);
+    }
+}
--- Assets/Scripts/Environment/TerminalInteractable.cs
+++ Assets/Scripts/Environment/TerminalInteractable.cs
@@ -5,23 +5,41 @@ using Interactables;
 public class TerminalInteractable : NetworkBehaviour
 {
     public GameObject[] Targets;
-    private GameObject _terminalModel;
+    public GameObject TerminalScreen;
     private InputListener _inputListener;
+
+    [Header("Rendering")]
+    private Renderer _screenRenderer;
+    private Color _targetColor;
+    private Color _currentColor;
+    private Color _startingColor;
 
     [Header("Debugging")]
     public bool ShowConnections;
 
     private void Start()
     {
-        _terminalModel = transform.GetChild(0).gameObject;
+        _screenRenderer = TerminalScreen.GetComponent<Renderer>();
+        _startingColor = _screenRenderer.material.color;
+        _currentColor = Color.black;
+        _targetColor = Color.black;
     }
 
     private void Update()
     {
-        if(_inputListener != null && _inputListener.UseKey)
+        _targetColor = Color.black;
+        if (_inputListener != null)
         {
-            Interactable.MessageTargets(Targets);
+           _targetColor = _startingColor;
+            if (_inputListener.UseKey)
+            {
+                Interactable.MessageTargets(Targets);
+            }
         }
+
+        _currentColor = Vector4.MoveTowards(_currentColor, _targetColor, Time.deltaTime);
+        _screenRenderer.material.color = _currentColor;
+        _screenRenderer.material.SetColor("_EmissionColor", _currentColor);
     }
 
     private void OnTriggerEnter(Collider other)

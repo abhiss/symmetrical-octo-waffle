f980c453306a44844411a46da7e3f7bf645853b2
Dominic Quintero <dominicaq09@gmail.com>
2023-05-22 18:38:58 -0700

    Updated Health system

--- Assets/Scripts/Character/CharacterInventory.cs
+++ Assets/Scripts/Character/CharacterInventory.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// namespace Items
+// {
+//     public enum ItemType
+//     {
+//         HealthModifier,
+//         AttackModifer,
+//         WeaponModifier,
+//         PlayerModifier
+//     }
+
+//     public interface IItem
+//     {
+
+//     }
+// }
+
 public class CharacterInventory : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Enemy/HealthSystem.cs
+++ Assets/Scripts/Enemy/HealthSystem.cs
@@ -1,9 +1,4 @@
-﻿using ParrelSync.NonCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using UnityEngine;
 
 namespace Shared
@@ -11,63 +6,95 @@ namespace Shared
 	public class HealthSystem : MonoBehaviour
 	{
 		[Header("Health Settings")]
-		public float maxHealth = 100.0f;
-		public float currentHealth;
+		public float MaxHealth = 100.0f;
+		public float CurrentHealth;
 
-		[Header("Damage Settings")]
-		public LayerMask enemyLayers;
-		public float damageOnCollision = 10.0f;
+		[Header("Shield Settings")]
+		public bool UseShield = false;
+		public float MaxShield = 100.0f;
+		public float CurrentShield;
+		public float RechargeDelay = 5.0f;
+		public float RechargeRate = 1.0f;
+		private float _rechargeTimer = 0.0f;
+		private bool _rechargeTimerReset = false;
 
+		// Event Handler
+		// ---------------------------------------------------------------------
 		public class OnDamageArgs : EventArgs
 		{
 			public float damage;
 			public float newHealth;
 			public GameObject attacker;
 		}
+
 		public event EventHandler<OnDamageArgs> OnDamageEvent;
-		// Start is called before the first frame update
-		void Start()
+
+		// ---------------------------------------------------------------------
+
+		public void BuffHealth(float multipler)
 		{
-			currentHealth = maxHealth;
+			float buffAmount = MaxHealth - MaxHealth * multipler;
+			MaxHealth += buffAmount;
+			CurrentHealth += buffAmount;
+		}
+
+		public void BuffShield(float multipler)
+		{
+			float buffAmount = MaxShield - MaxShield * multipler;
+			MaxShield += buffAmount;
+		}
+
+		private void Start()
+		{
+			CurrentHealth = MaxHealth;
+		}
+
+		private void Update()
+		{
+			Shield();
+		}
+
+		private void Shield()
+		{
+			if (_rechargeTimerReset)
+			{
+				_rechargeTimer = 0.0f;
+				_rechargeTimerReset = false;
+			}
+
+			if (_rechargeTimer < RechargeDelay)
+			{
+				return;
+			}
+
+			CurrentShield += Time.deltaTime * RechargeRate;
+			if (CurrentShield >= MaxShield)
+			{
+				CurrentShield = MaxShield;
+			}
 		}
 
 		/// <summary>
-		/// Used to apply damage to this ped
+		/// Used to apply damage to an HealthSystem instance.
 		/// </summary>
-		/// <param name="attacker"> The attacker doing the damage. Probably the object that the script calling this function is attached to. </param>
+		/// <param name="attacker"> Source of Damage. </param>
 		/// <param name="damage"> Amount of damage to apply to target. </param>
 		public void TakeDamage(GameObject attacker, float damage)
 		{
-			currentHealth -= damage;
-			OnDamageEvent.Invoke(this, new OnDamageArgs { damage = damage, newHealth = currentHealth, attacker = attacker });
-			//if (currentHealth <= 0)
-			//{
-			//	Die();
-			//}
+			_rechargeTimerReset = true;
+
+			CurrentHealth -= damage;
+			if (CurrentHealth <= 0)
+			{
+				Die();
+			}
+
+			OnDamageEvent.Invoke(this, new OnDamageArgs { damage = damage, newHealth = CurrentHealth, attacker = attacker });
 		}
 
-		//private void OnTriggerEnter(Collider other)
-		//{
-		//	// Check if the collided object is in the enemyLayers
-		//	if (((1 << other.gameObject.layer) & enemyLayers) != 0)
-		//	{
-		//		TakeDamage(damageOnCollision);
-		//	}
-		//}
+		private void Die()
+		{
 
-		//private void Die()
-		//{
-		//	// Implement any logic needed when the object dies, e.g., destroying the object
-		//	if (gameObject.CompareTag("TopDownCharacter"))
-		//	{
-		//		// Handle player death logic, e.g., showing Game Over screen, restarting the level, etc.
-		//		Debug.Log("Player died!");
-		//	}
-		//	else
-		//	{
-		//		// Handle enemy death logic
-		//		Destroy(gameObject);
-		//	}
-		//}
+		}
 	}
 }

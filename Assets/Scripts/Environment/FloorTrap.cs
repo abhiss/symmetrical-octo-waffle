bfe7d61a85065629027a0888e84ff9f532540271
Edward John <edjohn@Edwards-Air-4.attlocal.net>
2023-05-25 20:31:14 -0700

    Begin implementation of FloorTrap, currently unimplemented because I can't find a way to check for a player collider

---
+++ Assets/Scripts/Environment/FloorTrap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Shared;
+
+public class FloorTrap : MonoBehaviour
+{
+    [SerializeField] private float _damage = 2f;
+
+    void OnControllerColliderHit(ControllerColliderHit other)
+    {
+        Debug.Log("Triggered floor trap collider");
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            GameObject player = other.gameObject;
+            TriggerTrapOnPlayer(player);
+        }
+    }
+
+    private void TriggerTrapOnPlayer(GameObject player)
+    {
+        HealthSystem health = player.GetComponent<HealthSystem>();
+        health.TakeDamage(gameObject, _damage);
+    }
+}

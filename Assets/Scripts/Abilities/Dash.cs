52d502535aa9b829ac9829522f6df9300a288782
Dominic Quintero <dominicaq09@gmail.com>
2023-06-09 03:04:30 -0700

    Updated VFX

--- Assets/Scripts/Abilities/Dash.cs
+++ Assets/Scripts/Abilities/Dash.cs
@@ -4,10 +4,9 @@ using UnityEngine.VFX;
 
 public class Dash : NetworkBehaviour
 {
-    public GameObject VFXObject;
     public float DashForce = 10.0f;
-    private float _minPlaySpeed = 2.1f;
-    private VisualEffect _vfx;
+    private float _minPlaySpeed = 1.0f;
+    public VisualEffect DashVFX;
 
     [Header("Conditionals")]
     public bool IsDashing = false;
@@ -22,14 +21,13 @@ public class Dash : NetworkBehaviour
 
         _characterMotor = GetComponent<CharacterMotor>();
         _inputListener = GetComponent<InputListener>();
-        _vfx = VFXObject.GetComponent<VisualEffect>();
     }
 
     private void Update()
     {
         if (!IsOwner) return;
 
-        _vfx.playRate = Mathf.Max(_characterMotor.DashInputOverride.magnitude, _minPlaySpeed);
+        DashVFX.playRate = Mathf.Max(_characterMotor.DashInputOverride.magnitude, _minPlaySpeed);
 
         // Input
         if (_inputListener.ShiftKey && _characterMotor.isGrounded)
@@ -40,7 +38,7 @@ public class Dash : NetworkBehaviour
         // Stop the event once we reach the min play speed
         if (_characterMotor.DashInputOverride.magnitude <= _minPlaySpeed)
         {
-            _vfx.SendEvent(VisualEffectAsset.StopEventID);
+            DashVFX.SendEvent(VisualEffectAsset.StopEventID);
         }
 
         IsDashing = _characterMotor.DashInputOverride.magnitude > 1.0f;
@@ -58,6 +56,6 @@ public class Dash : NetworkBehaviour
     {
         Vector3 dashVector = transform.forward.normalized;
         _characterMotor.DashInputOverride = dashVector * DashForce;
-        _vfx.SendEvent(VisualEffectAsset.PlayEventID);
+        DashVFX.SendEvent(VisualEffectAsset.PlayEventID);
     }
 }

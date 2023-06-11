c9db7c3ac21dc3000aeb3511da5c526a68ea7d96
Edward John <54566278+edjohn@users.noreply.github.com>
2023-06-11 10:59:48 -0700

    Multiplayer adjustments and gizmo debugging for hazards (#36)
    * Fix turret and floortrap to work correctly with multiple players
    
    * Initialize hazard prefabs with correct values; Add debugging gizmos to turret and explosion

--- Assets/Scripts/Hazards/Explosion.cs
+++ Assets/Scripts/Hazards/Explosion.cs
@@ -7,35 +7,39 @@ using Shared;
 public class Explosion : NetworkBehaviour
 {
     [SerializeField] private float _explosionDamage;
-    [SerializeField] private float _explosionRadius = 2f;
+    [SerializeField] private float _explosionRadius;
     private const float _explosionDuration = 2f;
+    [SerializeField] private LayerMask _targetMask;
     private AudioSource _explosionAudio;
+
+    [Header("Gizmos")]
+    [SerializeField] private bool _showExplosionRadius;
 
     private void Start()
     {
         _explosionAudio = GetComponent<AudioSource>();
-        // Explode as soon as the explosion is instantiated.
         Explode();
     }
 
     private void Explode()
     {
-        // Play an explosion sound effect.
         _explosionAudio.Play();
-        // Detect all colliders within a explosion radius.
-        Collider[] collidersInExplosionRadius = Physics.OverlapSphere(transform.position, _explosionRadius);
+        // Deal damage to all objects in target mask within explosion radius.
+        Collider[] collidersInExplosionRadius = Physics.OverlapSphere(transform.position, _explosionRadius, _targetMask);
         foreach (Collider collider in collidersInExplosionRadius)
         {
-            // Can be succinctly converted to a layer mask if more interactions become possible.
-            bool isValidTarget = collider.CompareTag("Player") || collider.CompareTag("Enemy") || collider.CompareTag("Destructible");
-            // If a collider is a valid target, deal damage to its health.
-            if (isValidTarget)
-            {
-                HealthSystem targetHealth = collider.gameObject.GetComponent<HealthSystem>();
-                targetHealth.TakeDamage(gameObject, _explosionDamage);
-            }
+            HealthSystem targetHealth = collider.gameObject.GetComponent<HealthSystem>();
+            targetHealth.TakeDamage(gameObject, _explosionDamage);
         }
-        // Destroy the explosion after a duration.
         Destroy(gameObject, _explosionDuration);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_showExplosionRadius)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, _explosionRadius);
+        }
     }
 }

70248dbcc636206f3ea51b05d63634ca978aa4b1
TSYW01 <129975299+TSYW01@users.noreply.github.com>
2023-06-12 15:16:49 -0700

    trying weapon pickup

--- Assets/Scripts/Character/CharacterWeaponPickUp.cs
+++ Assets/Scripts/Character/CharacterWeaponPickUp.cs
@@ -3,49 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
 public class CharacterWeaponPickUp : MonoBehaviour
 {
-    private CharacterShooting characterShooting; 
-    public Transform WeaponHolder;
-    private WeaponCreator currentWeapon;
+    private CharacterShooting characterShooting;
+    public float pickupRadius = 3f; // Radius within which the player can pick up the weapon
+    public WeaponCreator currentWeapon;
     private Transform weaponTransform;
-    private WeaponCreator newWeapon;
+    public WeaponCreator newWeapon;
 
     private void Start()
     {
-        // Get a reference to the CharacterShooting script
         characterShooting = GetComponent<CharacterShooting>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player"))
         {
-            newWeapon = other.GetComponent<WeaponCreator>();
-            if (newWeapon != null)
-            {
-                // Pick up the new weapon
-                characterShooting.SetActiveWeapon(newWeapon);
-
-                // Disable the weapon object in the scene
-                other.gameObject.SetActive(false);
-            }
+            return;
         }
+
+        newWeapon = other.GetComponent<WeaponCreator>();
+        if (newWeapon != null)
+        {
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            if (distance <= pickupRadius)
+            {
+                characterShooting.SetActiveWeapon(newWeapon);
+                other.gameObject.SetActive(false);
+                Debug.Log("SetActiveWeapon");
+            }
+            Debug.Log("Weapon Not Null");
+        }
+        else
+        {
+            Debug.Log("Weapon Null");
+        }
+        Debug.Log("Trigger Executed!");
+
     }
 
+    /*
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                float distance = Vector3.Distance(transform.position, other.transform.position);
+                if (distance <= pickupRadius)
+                {
+                    newWeapon = other.GetComponent<WeaponCreator>();
+                    if (newWeapon != null)
+                    {
+                        characterShooting.SetActiveWeapon(newWeapon);
+                        other.gameObject.SetActive(false);
+                        Debug.Log("SetActiveWeapon");
+                    }
+                }
+            }
+        }
+    */
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown("e"))
         {
             if (currentWeapon != null)
             {
-                // Drop the current weapon
                 DropWeapon();
+                Debug.Log("You have dropped your weapon");
             }
             else
             {
-                // Pick up the nearest weapon from the ground
                 PickUpWeapon(newWeapon);
+                Debug.Log("You have picked up the weapon");
             }
         }
     }
@@ -54,7 +88,7 @@ public class CharacterWeaponPickUp : MonoBehaviour
     {
         if (currentWeapon != null)
         {
-            DropWeapon(); // Drop the current weapon before picking up a new one
+            DropWeapon();
         }
 
         currentWeapon = newWeapon;
@@ -67,8 +101,5 @@ public class CharacterWeaponPickUp : MonoBehaviour
             currentWeapon = null;
         }
     }
- 
-
 }
-
 

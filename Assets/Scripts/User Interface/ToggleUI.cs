51489b379e4fbd8bdc3f54582f60c8d8a2d951e7
dragon2601 <92962702+dragon2601@users.noreply.github.com>
2023-06-13 14:12:20 -0700

    pause menu added to main scene. Added script and text object for invalid code
    #14 #17

--- Assets/Scripts/User Interface/ToggleUI.cs
+++ Assets/Scripts/User Interface/ToggleUI.cs
@@ -6,7 +6,6 @@ public class ToggleUI : MonoBehaviour
 {
     [SerializeField] GameObject[] objectsToToggle;
     [SerializeField] KeyCode[] inputKeys;
-    [SerializeField] PauseMenu pauseMenuScript; // Reference to the PauseMenu script
 
     void Update()
     {
@@ -16,27 +15,6 @@ public class ToggleUI : MonoBehaviour
             {
                 bool activeState = objectsToToggle[i].activeInHierarchy;
                 objectsToToggle[i].SetActive(!activeState);
-
-                // Call the TogglePause method from the PauseMenu script
-                if (objectsToToggle[i].name == "Pause Menu") // Make sure to match the name of your PauseMenu GameObject
-                {
-                    pauseMenuScript.TogglePause();
-                }
-            }
-        }
-    }
-
-    // Add this new method to the ToggleUI script
-    public void TogglePauseMenu()
-    {
-        for (int i = 0; i < objectsToToggle.Length; i++)
-        {
-            if (objectsToToggle[i].name == "Pause Menu") // Make sure to match the name of your PauseMenu GameObject
-            {
-                bool activeState = objectsToToggle[i].activeInHierarchy;
-                objectsToToggle[i].SetActive(!activeState);
-                pauseMenuScript.Pause(!activeState);
-                break;
             }
         }
     }

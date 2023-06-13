714ddd3ade89179fcb421b565bc26d70a29e7cca
dragon2601 <92962702+dragon2601@users.noreply.github.com>
2023-06-13 12:29:54 -0700

    worked on getting a car system up, still has errors
    #45 #14

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,46 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Vector2 rawInput;
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float paddingLeft = 5f;
-    [SerializeField] float paddingRight = 5f;
-    [SerializeField] float paddingTop = 5f;
-    [SerializeField] float paddingBottom = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private GameObject player;
+    [SerializeField] private SphereCollider entryArea;
 
-    Vector2 minBounds;
-    Vector2 maxBounds;
-    void Start()
+    private Rigidbody rb;
+    private bool playerInside = false;
+    private bool playerNearby = false;
+
+    private void Awake()
     {
-        InitBounds();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void InitBounds()
+    private void Update()
     {
-        Camera mainCamera = Camera.main;
-        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0f));
-        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1f));
+        // Read inputs
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input.Normalize();
+
+        // If player is nearby and presses the 'E' key, toggle whether they are inside the car
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            playerInside = !playerInside;
+            player.SetActive(!playerInside);
+        }
+
+        if (!playerInside)
+            return;
+
+        // Calculate rotation amount
+        float rotationAmount = input.x * rotationSpeed * Time.deltaTime;
+
+        // Rotate the car
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, rotationAmount, 0));
+
+        // Calculate forward movement
+        Vector3 forward = transform.forward;
+        float speedAmount = input.y * speed * Time.deltaTime;
+
+        // Move the car
+        rb.MovePosition(rb.position + forward * speedAmount);
     }
-    void Move()
+
+    private void OnTriggerEnter(Collider other)
     {
-        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
-        Vector2 newPos = new Vector2();
-        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x+paddingLeft, maxBounds.x-paddingRight);
-        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y+paddingBottom, maxBounds.y-paddingTop);
-        transform.position = newPos;
+        if (other.gameObject == player)
+        {
+            playerNearby = true;
+            Debug.Log("Player has entered the car's radius.");
+        }
     }
-    void OnMove(InputValue value)
+
+    private void OnTriggerExit(Collider other)
     {
-        rawInput = value.Get<Vector2>();
-        Debug.Log(rawInput);
+        if (other.gameObject == player)
+        {
+            playerNearby = false;
+            // If the player was inside the car but moves out of range, eject them from the car
+            if (playerInside)
+            {
+                playerInside = false;
+                player.SetActive(true);
+            }
+            Debug.Log("Player has left the car's radius.");
+        }
     }
 }
